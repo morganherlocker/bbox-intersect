e97e665bf514531cdd4301a291f8f524532188fd
morganherlocker <morgan.herlocker@gmail.com>
2014-08-11 15:50:36 -0400

    implemented passing tests

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 module.exports = function(bbox1, bbox2){
-	
+  if(!(
+      bbox1[0] > bbox2[2] ||
+      bbox1[2] < bbox2[0] ||
+      bbox1[3] < bbox2[1] ||
+      bbox1[1] > bbox2[3]
+    )){
+      return true;
+  } else {
+    return false;
+  }
 }
